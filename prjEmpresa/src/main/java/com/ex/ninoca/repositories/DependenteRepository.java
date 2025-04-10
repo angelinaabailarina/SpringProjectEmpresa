af5132f6e49a8de630c9273d30814b8874e4a2ef
angelinaabailarina <angelina.almeida.dev2024@gmail.com>
2025-04-10 07:48:57 -0300

    Add files via upload

---
+++ prjEmpresa/src/main/java/com/ex/ninoca/repositories/DependenteRepository.java
@@ -0,0 +1,9 @@
+package com.ex.ninoca.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ex.manu.entities.Dependente;
+
+public interface DependenteRepository extends JpaRepository<Dependente, Long>{
+
+}
---
+++ prjEmpresa/src/main/java/com/ex/ninoca/repositories/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+package com.ex.ninoca.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ex.manu.entities.Funcionario;
+
+public interface FuncionarioRepository extends JpaRepository<com.ex.ninoca.entities.Funcionario, Long>{
+
+}
